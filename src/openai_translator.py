743e9427d89ccb12311bdc6adf9717eaee8deabb
Waqqas <76067933+Waqqas41@users.noreply.github.com>
2023-12-22 17:18:22 -0500

    Update openai_translator.py

--- src/openai_translator.py
+++ src/openai_translator.py
@@ -15,7 +15,7 @@ def read_txt_file(file_path):
 txt_file_path = 'all_output.txt'
 
 # Define the system message and user message
-system_message = "You are an Alim. You specialize in accurately translating Islamic books to an english audience. You will be provided with raw Arabic. your task is to translate it into English."
+system_message = "You are an Alim. You specialize in accurately translating Islamic books to english. You will be provided with raw Arabic. For arabic terms or words or names you absolutely must put diacritics for vowels for example Ā. translate to english and write in the style of Sunni Islamic books."
 
 # Read content from the .txt file
 user_content = read_txt_file(txt_file_path)
